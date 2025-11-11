8c21ca47e38600f6f5d4da8caa5cac2e289a9ffc
DaveLee-b <lpoln1855@gmail.com>
2025-11-11 17:28:26 +0900

    기존학습내용 깃허브push

---
+++ src/main/java/com/kt/controller/order/AdminOrderController.java
@@ -0,0 +1,32 @@
+package com.kt.controller.order;
+
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kt.common.ApiResult;
+import com.kt.common.Paging;
+import com.kt.dto.order.OrderResponse;
+import com.kt.repository.order.OrderRepository;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/admin/orders")
+@RequiredArgsConstructor
+public class AdminOrderController {
+	private final OrderRepository orderRepository;
+
+	// 서비스는 컨트롤러에서 하는일x
+
+	@GetMapping
+	public ApiResult<Page<OrderResponse.Search>> search(
+		@RequestParam(required = false) String keyword,
+		@Parameter(hidden = true) Paging paging
+	) {
+		return ApiResult.ok(orderRepository.search(keyword, paging.toPageable()));
+	}
+}
